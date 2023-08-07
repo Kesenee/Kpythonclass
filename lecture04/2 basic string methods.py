6b9c5d6b15999bdddc3f7d0a7acd0e92b8d88bcb
Kesenee <keseneesanee@gmail.com>
2023-08-07 11:40:46 +0700

    Kesenee 7/8/23

---
+++ lecture04/2 basic string methods.py
@@ -0,0 +1,10 @@
+filename = input('Enter the filename :')
+if filename.endswith('.txt') :
+    print('That is name of a text file.')
+elif filename.endswith ('.py') :
+    print('That is the name of a python source file')
+elif filename.endswith('.doc') :
+    print('That is the name of a word processing document.')
+else:
+    print('Unknow file type.')
+
