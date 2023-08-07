073a4fed0919d223a34aa696bceeb47583f7ee18
Kesenee <keseneesanee@gmail.com>
2023-08-07 10:04:07 +0700

    Kesenee

---
+++ lecture04/exercise1.py
@@ -0,0 +1,7 @@
+first_name = input('Enter your first name :')
+last_name = input('Enter your last name :')
+studen_ID_number = int(input('Enter your studen ID number :'))
+print(first_name[:3]) + (last_name[:3]) + (studen_ID_number[3:])
+
+
+ยังไม่เสร็จ หน้า 16 lecture04
