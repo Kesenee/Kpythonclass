2a2ea95d88e2b2f2bebd6c5a5fb0fea1934083b1
Kesenee <keseneesanee@gmail.com>
2023-08-07 11:35:13 +0700

    Kesenee 7/8/23

---
+++ lecture04/basic string methods.py
@@ -0,0 +1,9 @@
+date_string = '11/26/2014'
+date_list = date_string.split('/')
+
+print('Month:', date_list[0])
+print('Day:', date_list[1])
+print('Year:', date_list[2])
+print(date_list)
+if (date_list[0]) + (date_list[1]) + (date_list[2]) :
+    print('november')
