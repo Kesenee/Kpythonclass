352d2fcd900193c17d19127aa3ee286f7e2426ae
Kesenee <keseneesanee@gmail.com>
2023-08-16 09:44:58 +0700

    Kesenee

---
+++ lecture06/filewrite.py
@@ -0,0 +1,10 @@
+def main() :
+    outfile = open('philosophers.txt', 'w')
+
+    outfile.write('John Locke\n')
+    outfile.write('DAvid Hume\n')
+    outfile.write('Edmund Burke\n')
+
+    outfile.close()
+
+    main()
