1cf9797b11708bf41e91f4570655b348c77c308c
Kesenee <keseneesanee@gmail.com>
2023-08-21 10:48:06 +0700

    Kesenee

---
+++ lecture06/try-except-finally.py
@@ -0,0 +1,16 @@
+def main() :
+    a,b = map(int, input("Input 2 inteer values: ").split())
+    divide(a,b)
+
+def divide(x,y) :
+
+    try:
+         result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("executing finally clause")
+
+main()
